d6cf588983e977200a947aecb49499359a1a097b
Artem Dirizhenko <dekate33@outlook.com>
2024-01-16 22:59:55 +0300

    add file reviews.urls.py, add model Review

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
@@ -32,3 +33,22 @@ class Category(models.Model):
 
 class CustomUser(AbstractUser):
     bio = models.TextField('Биография', blank=True)
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='reviews'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='reviews'
+    )
+    score = models.IntegerField(
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ])
+    pub_date = models.DateTimeField('Дата создания', auto_now_add=True)
+
+    def __str__(self):
+        return self.name
