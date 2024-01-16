f99aa761df1899c9295532b7fa7d27c8c6baedeb
Антон Арефин <r4zeel@yandex.ru>
2024-01-16 13:06:47 +0300

    added def __str__ for title model and max_lenght param for name field

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 
 class Title(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=28)
     year = models.DateField()
     rating = models.IntegerField()
     description = models.TextField()
     genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Genre(models.Model):
