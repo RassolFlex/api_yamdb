0ba9cceff369e00ad012e8bcfaefeaab0b4651e2
Антон Арефин <r4zeel@yandex.ru>
2024-01-16 13:04:32 +0300

    added Title model. Also added empty Genre and Category models

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Title(models.Model):
+    name = models.CharField()
+    year = models.DateField()
+    rating = models.IntegerField()
+    description = models.TextField()
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+
+class Genre(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
