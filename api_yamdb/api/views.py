de8acf577096ebe8c894a7f3a5852959295fb8b0
Антон Арефин <r4zeel@yandex.ru>
2024-01-17 21:43:32 +0300

    added Genre and Category serializers and viewsets, added slugrelatedfields to Title serializer

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,19 @@
 from rest_framework import viewsets
 
-from reviews.models import Title
-from .serializers import TitleSerializer
+from reviews.models import Title, Genre, Category
+from .serializers import TitleSerializer, GenreSerializer, CategorySerializer
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
