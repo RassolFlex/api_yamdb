acfb2d9d9d6199c3f771ba481c57ef58a541f8cb
Антон Арефин <r4zeel@yandex.ru>
2024-01-20 19:24:36 +0300

    fixed error with duplicating values

--- api_yamdb/reviews/management/commands/csv_import.py
+++ api_yamdb/reviews/management/commands/csv_import.py
@@ -10,14 +10,15 @@ class Command(BaseCommand):
     def handle(self, **options):
         csv_data = []
         for file in listdir('static/data/'):
-            model_dict = dict()
-            model_dict['model'] = f'reviews.{Path(file).stem}'
             dataframe = pandas.read_csv(Path(f'static/data/{file}'))
             json_file = dataframe.to_json(orient='records')
             parsed = json.loads(json_file)
-            pk = parsed[0].pop('id')
-            model_dict['pk'] = pk
-            model_dict['fields'] = parsed[0]
-            csv_data.append(model_dict)
+            for index, item in enumerate(parsed):
+                model_dict = dict()
+                model_dict['model'] = f'reviews.{Path(file).stem}'
+                pk = parsed[index].pop('id')
+                model_dict['pk'] = pk
+                model_dict['fields'] = parsed[index]
+                csv_data.append(model_dict)
         with open('data.json', 'w') as f:
             json.dump(csv_data, f, ensure_ascii=False, indent=4)
