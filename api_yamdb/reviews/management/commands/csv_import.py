617553baf9340e23f86862042e143ad61a3e40a1
Антон Арефин <r4zeel@yandex.ru>
2024-01-20 18:40:25 +0300

    added script for parsing csv files into single json

--- api_yamdb/reviews/management/commands/csv_import.py
+++ api_yamdb/reviews/management/commands/csv_import.py
@@ -1,5 +1,21 @@
+import pandas
+import json
+
 from django.core.management.base import BaseCommand
+from pathlib import Path
+from os import listdir
+
 
 class Command(BaseCommand):
     def handle(self, **options):
-        pass
+        csv_data = []
+        for file in listdir('static/data/'):
+            model_dict = dict()
+            model_dict['model'] = f'reviews.{Path(file).stem}'
+            dataframe = pandas.read_csv(Path(f'static/data/{file}'))
+            json_file = dataframe.to_json(orient='records')
+            parsed = json.loads(json_file)
+            model_dict['fields'] = parsed[0]
+            csv_data.append(model_dict)
+        with open('data.json', 'w') as f:
+            json.dump(csv_data, f, ensure_ascii=False, indent=4)
