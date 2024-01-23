6b91330516351d8280bc89d95c0d828aecdec8f6
Александр Огольцов <26kotov@gmail.com>
2024-01-23 19:35:04 +0300

    fix warnings in pytest tests

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -38,6 +38,9 @@ class Category(models.Model):
     name = models.CharField(max_length=256)
     slug = models.SlugField(unique=True, max_length=50)
 
+    class Meta:
+        ordering = ('name',)
+
     def __str__(self):
         return self.slug
 
