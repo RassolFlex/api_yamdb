ddf16ee09e57ff0c9d49ae0fffe89b5c6e231cf9
Антон Арефин <r4zeel@yandex.ru>
2024-01-26 13:55:24 +0300

    fixed filter search

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -1,13 +1,19 @@
 from django_filters import rest_framework as filters
+from django.db.models import Q
 
 from reviews.models import Title
 
 
 class TitleSearchFilter(filters.FilterSet):
-    genre = filters.CharFilter(field_name='genre')
-    category = filters.CharFilter(field_name='category')
-    year = filters.CharFilter(field_name='year')
+    name = filters.CharFilter(field_name='name', lookup_expr='icontains')
+    genre = filters.CharFilter(field_name='genre', method='get_slug')
+    category = filters.CharFilter(field_name='category', method='get_slug')
+    year = filters.NumberFilter(field_name='year')
 
     class Meta:
         model = Title
-        fields = ['genre', 'year', 'category']
+        fields = ['name', 'genre', 'year', 'category']
+
+    def get_slug(self, queryset, name, value):
+        name = f'{name}__slug'
+        return queryset.filter(Q((name, value)))
--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,9 +6,9 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated)
 
-    def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.role == 'admin')
+    # def has_object_permission(self, request, view, obj):
+    #     return (request.method in permissions.SAFE_METHODS
+    #             or request.user.role == 'admin')
 
 
 class AdminOnly(permissions.BasePermission):
